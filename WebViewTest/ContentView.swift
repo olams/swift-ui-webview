30cb653e1efd36755ced51c7faa6f4c1162c584d
Ola Marius Hoff Sagli <ola@sagliconsulting.com>
2020-07-28 19:57:16 +0200

    Initial Commit

---
+++ WebViewTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebViewTest
+//
+//  Created by Ola Marius Sagli on 28/07/2020.
+//  Copyright © 2020 Ola Marius Sagli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
