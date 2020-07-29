0a3d9504f34f41b246db8f37882baa2ef5c8cad9
Ola Marius Hoff Sagli <ola@sagliconsulting.com>
2020-07-29 10:37:25 +0200

    Add list and rows
    Show a list with webviews wrapped in view that supports dynamic height.
    Use @State and @Binding to update the frame height to match the webview height.
    
    - Add webview in list
    - Support dynamic height
    
    TODO:  Improve scrolling

--- WebViewTest/ContentView.swift
+++ WebViewTest/ContentView.swift
@@ -10,21 +10,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        
-        VStack {
-            
-            Text("Headline").font(.headline)
-            
-            Text("Body").font(.body)            
-            
-            MyWebView()
-        }
+  var body: some View {
+    
+    List {
+      WebViewRow(fileName: "test")
+      WebViewRow(fileName: "test2")
+      WebViewRow(fileName: "test")
+      WebViewRow(fileName: "test2")
+      WebViewRow(fileName: "test")
+      WebViewRow(fileName: "test2")
+      WebViewRow(fileName: "test")
+      WebViewRow(fileName: "test2")
+
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- WebViewTest/MyWebView.swift
+++ WebViewTest/MyWebView.swift
@@ -11,72 +11,74 @@ import Foundation
 import SwiftUI
 import WebKit
 
+// Resizable list https://stackoverflow.com/questions/58060530/swiftui-resizable-list-height-that-dependent-on-an-element-count
+struct WebViewRow : View {
+  
+  @State private var webViewHeight: CGFloat = .zero
+  var fileName:String
+  
+  var body: some View {
+
+    VStack {
+      MyWebView(fileName: fileName, dynamicHeight: $webViewHeight)
+    }.frame(height: CGFloat(webViewHeight))
+  }
+  
+}
 
 // View holding article
 struct MyWebView : UIViewRepresentable {
     
+  
+    var fileName:String
     let webConfiguration = WKWebViewConfiguration()
     let contentController = WKUserContentController()
-    
-    /*
-    func createCssScript(cssFile:String) -> WKUserScript {
-        
-        let cssPath = Bundle.main.path(forResource: cssFile, ofType: "css")!
-        let cssScriptString = """
-            var link = document.createElement('link');
-            link.href = '\(cssPath)';
-            link.rel = 'stylesheet';
-            document.head.appendChild(link);
-        """
-        
-        return WKUserScript(source: cssScriptString, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-    }
-     */
-    
+    @Binding var dynamicHeight: CGFloat
+          
     func makeUIView(context: Context) -> WKWebView {
-        
         let webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        /*
-        let dictCssScript = createCssScript(cssFile: "dictionaries-v2")
-        let accessibilityCss = createCssScript(cssFile: "accessibility")
-        */
-        // webConfiguration.userContentController.addUserScript(dictCssScript)
-        // ebConfiguration.userContentController.addUserScript(accessibilityCss)
+        webView.scrollView.isScrollEnabled = false
+        webView.scrollView.panGestureRecognizer.isEnabled = false;
+        webView.scrollView.bounces = false;
         return webView
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        
-        // uiView.navigationDelegate = context.coordinator
-        
-        let path = Bundle.main.path(forResource: "test", ofType: "html")
+
+        uiView.navigationDelegate = context.coordinator
+        let path = Bundle.main.path(forResource: fileName, ofType: "html")
         let request = URLRequest(url: URL(fileURLWithPath: path!))
         //let request = URLRequest(url: URL(string: article.HTMLcollapsedURL)!)
         uiView.load(request)
     }
 
-    /*
     class Coordinator : NSObject, WKNavigationDelegate {
         
-        var parent: ArticleView
+        var parent: MyWebView
         
-        init(_ parent: ArticleView) {
+        init(_ parent: MyWebView) {
             self.parent = parent
         }
         
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            
+            webView.evaluateJavaScript("document.readyState", completionHandler: { (complete, error) in
+                     if complete != nil {
+                         webView.evaluateJavaScript("document.body.scrollHeight", completionHandler: { (height, error) in
+                          self.parent.dynamicHeight = webView.scrollView.contentSize.height
+                         })
+                     }
+                 })
         }
     }
 
     func makeCoordinator() -> Coordinator {
         return Coordinator(self)
     }
-    */
 }
 
 struct MyWebView_Previews: PreviewProvider {
     static var previews: some View {
-        
         Text("Hello, World asdf!")
     }
 }
